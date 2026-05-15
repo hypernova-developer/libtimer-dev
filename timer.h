27b2eaffb580fbe3d6a5e865ccbabca8f06e768e
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-05-15 22:00:24 +0300

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,1 +1,21 @@
+#pragma once
 
+#ifndef TIMER_H
+#define TIMER_H
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+static inline void sys_sleep(unsigned int ms)
+{
+    #ifdef _WIN32
+    Sleep(ms);
+    #else
+    usleep(ms * 1000);
+    #endif
+}
+
+#endif
